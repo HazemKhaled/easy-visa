4aadcce5cfb37f2bfa11542e848230cd52fe9658
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-03-28 22:12:07 +0300

    style: add logo

---
+++ src/components/Navbar/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className=' text-cyan-500 font-semibold text-xl '>Easy Visa</h1>
+    </div>
+  );
+};
+
+export default Logo;
