05daae931aed303c946a61b44545e7613d4446cb
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-04-06 18:47:49 +0300

    style: clean up

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,8 @@
-import { useTranslations } from 'next-intl';
-
 import AboutPage from '@/app/[locale]/page/about/page';
 
 import Navbar from '../../components/Navbar/Navbar';
 
 export default function Index() {
-  const t = useTranslations('Index');
-
   return (
     <div>
       <Navbar />
