9b237cb73fe8dfb2d4768fc5df311a81515a9002
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-04-07 08:20:24 +0300

    style: navbar stick at the top

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo';
 
 const Navbar = () => {
   return (
-    <div className='w-full h-20 flex items-center justify-between px-10 bg-zinc-200 shadow-lg '>
+    <div className=' fixed w-full h-20 flex items-center justify-between px-10 bg-zinc-200 shadow-lg '>
       <div>
         <Logo />
       </div>
