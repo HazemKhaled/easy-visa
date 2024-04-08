bd9409921bafe4c317e8735e6a87302a382ea392
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-04-08 17:06:29 +0300

    fix: build fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,3 @@
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 // Can be imported from a shared config
@@ -6,7 +5,6 @@ export const locales = ['en', 'ar'];
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) notFound();
 
   return {
     messages: (
