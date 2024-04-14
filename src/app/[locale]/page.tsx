b3c721a7812524303a9a8d1e2f3d4c36f16b8afd
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-04-14 16:29:12 +0300

    style: add not found page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,14 @@
-import PrivacyPage from '@/app/[locale]/page/privacy/page';
+
+import { useTranslations } from 'next-intl';
 
 import Navbar from '../../components/Navbar/Navbar';
 
 export default function Index() {
+  const t = useTranslations('Index');
   return (
     <div>
       <Navbar />
-      <PrivacyPage />
+      <h1 className=' pt-[100px] '>{t('title')} </h1>
     </div>
   );
 }
