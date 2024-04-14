28fe734e33612243692bc016b9eaa72e55b1ba53
Khaled Hazem <khaled.hazem2009@gmail.com>
2024-04-14 19:44:13 +0300

    style: add href to a reusable component

--- src/components/Navbar/NavBarBtn.tsx
+++ src/components/Navbar/NavBarBtn.tsx
@@ -4,21 +4,30 @@ const NavbarBtn = () => {
   const t = useTranslations('Layout');
 
   return (
-    <div className=' flex col-span-4 gap-10 text-gray-500 '>
-      <NavBtn>{t('AboutNavBtn')}</NavBtn>
-      <NavBtn>{t('ContactUsNavBtn')}</NavBtn>
-      <NavBtn>{t('PrivacyNavBtn')}</NavBtn>
-      <NavBtn>{t('TermsNavBtn')}</NavBtn>
+    <div className='flex col-span-4 gap-10 text-gray-500'>
+      <NavBtn href='/about'>{t('AboutNavBtn')}</NavBtn>
+      <NavBtn href='/contact'>{t('ContactUsNavBtn')}</NavBtn>
+      <NavBtn href='/privacy'>{t('PrivacyNavBtn')}</NavBtn>
+      <NavBtn href='/terms'>{t('TermsNavBtn')}</NavBtn>
     </div>
   );
 };
 
 export default NavbarBtn;
 
-const NavBtn = ({ children }: { children: string | JSX.Element }) => {
+const NavBtn = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: string | JSX.Element;
+}) => {
   return (
-    <button className=' h-8 px-1 hover:text-cyan-500 rounded-lg hover:font-semibold hover:scale-105 duration-200'>
+    <a
+      href={href}
+      className='h-8 px-1 hover:text-cyan-500 rounded-lg hover:font-semibold hover:scale-105 duration-200'
+    >
       {children}
-    </button>
+    </a>
   );
 };
