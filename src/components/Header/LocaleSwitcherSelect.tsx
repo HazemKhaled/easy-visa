7dd37e6c65ba289272f4a79af621c3c27dfc35f4
Hazem Khaled <hazem.khaled@gmail.com>
2024-04-18 00:53:14 +0300

    fix: switch language
    Signed-off-by: Hazem Khaled <hazem.khaled@gmail.com>

--- src/components/Header/LocaleSwitcherSelect.tsx
+++ src/components/Header/LocaleSwitcherSelect.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import clsx from 'clsx';
-import { useParams } from 'next/navigation';
 import { ChangeEvent, ReactNode, useTransition } from 'react';
 
 import { usePathname, useRouter } from '@/lib/navigation';
@@ -18,20 +17,14 @@ export default function LocaleSwitcherSelect({
   label,
 }: Props) {
   const router = useRouter();
+
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
-  const params = useParams();
 
   function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
     const nextLocale = event.target.value;
     startTransition(() => {
-      router.replace(
-        // @ts-expect-error -- TypeScript will validate that only known `params`
-        // are used in combination with a given `pathname`. Since the two will
-        // always match for the current route, we can skip runtime checks.
-        { pathname, params },
-        { locale: nextLocale },
-      );
+      router.replace(pathname, { locale: nextLocale });
     });
   }
 
